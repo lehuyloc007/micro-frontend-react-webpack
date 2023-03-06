ffb1cf3758c081c1b7a8f3e73d1f92bb83cee4a8
lehuyloc007 <79748205+lehuyloc007@users.noreply.github.com>
2023-03-06 17:20:57 +0700

    add react-router dom

--- host-app/src/App.js
+++ host-app/src/App.js
@@ -16,6 +16,7 @@ const RemoteWrapper = ({ children }) => (
 export const App = () => (
  <div style={{ background: "rgba(43, 192, 219, 0.3)" }}>
    <h1>This is the Host!</h1>
+   <div>Header Footer Sidebar</div>
    <h2>Remote App:</h2>
    <RemoteWrapper>
      <RemoteApp />
